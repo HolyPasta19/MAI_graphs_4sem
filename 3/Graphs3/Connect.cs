59cdea9ac26c46da58a4c8bfb8e0d1c89fa49ed2
HolyPasta19 <64472259+HolyPasta19@users.noreply.github.com>
2023-10-29 20:45:42 +0300

    Add files via upload

---
+++ 3/Graphs3/Connect.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs3
+{
+    using Vertex = System.Int32;
+    public static class Connect
+    {
+        public static List<HashSet<Vertex>> IsConnectNotOriented(this Graph_int graph)
+        {
+            bool[] visitedVertex = new bool[graph.VertexCount()];
+            var connectedComponent = new List<HashSet<Vertex>>();
+            connectedComponent.Add(new HashSet<Vertex>());
+            var start = 0;
+            var queue = new Queue<Vertex>();
+            int i = 0;
+            //visitedVertex[i] = true;
+
+            var newgraph = GetOriented(graph);
+            queue.Enqueue(start);
+            while (visitedVertex.Any(v => v == false))
+            {
+                while (queue.Count() != 0)
+                {
+                    var current = queue.Dequeue();
+                    connectedComponent[i].Add(current);
+                    visitedVertex[current] = true;
+                    foreach (var next in newgraph.AdjacencyList(current))
+                    {
+                        if (!connectedComponent[i].Contains(next))
+                        {
+                            queue.Enqueue(next);
+                        }
+                    }
+                }
+                for (int j = 0; j < graph.VertexCount(); j++)
+                {
+                    if (visitedVertex[j] == false)
+                    {
+                        i++;
+                        connectedComponent.Add(new HashSet<Vertex>());
+                        queue.Enqueue(j);
+                        break;
+                    }
+                }
+            }
+
+
+        
+            
+            return connectedComponent;
+        }
+        
+        public static Graph_int GetOriented( Graph_int graph)
+        {
+            var orMatrix = graph.AdjacencyMatrix();
+            for (int i = 0; i<graph.VertexCount();i++)
+            {
+                for (int j = 0; j < graph.VertexCount(); j++)
+                {
+                    if (orMatrix[i, j] != orMatrix[j, i] && orMatrix[j,i]!=0)
+                    {
+                        orMatrix[i,j] = orMatrix[j, i];
+                    }
+                    if (orMatrix[j, i] != orMatrix[i, j] && orMatrix[i, j] != 0)
+                    {
+                        orMatrix[j, i] = orMatrix[i, j];
+                    }
+                }
+            }
+            return new GraphMatrix(orMatrix);
+        }
+
+    }
+}
---
+++ 3/Graphs3/Graph_int.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs3
+{
+    using Vertex = System.Int32;
+    public interface Graph_int
+    {
+        public int Weight(Vertex vi, Vertex vj);
+        public bool IsEdge(Vertex vi, Vertex vj);
+        public int[,] AdjacencyMatrix();
+        public List<int> AdjacencyList(Vertex v);
+        public List<(int, int, int)> List_Of_Edges();
+        public List<(int, int, int)> List_Of_Edges(Vertex v);
+        public bool IsDirected();
+        public int VertexCount();
+    }
+}
