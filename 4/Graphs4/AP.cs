2ae79ce98f95745e47916d8ef2f6e5fc6d743d42
HolyPasta19 <64472259+HolyPasta19@users.noreply.github.com>
2023-10-29 20:46:26 +0300

    Add files via upload

---
+++ 4/Graphs4/AP.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs4
+{
+    static public class AP
+    {
+        static private HashSet<int> ap= new HashSet<int>();
+        static int time = 0;
+        
+
+        static void APUtil
+            (
+                Graph_int graph,
+                int u, 
+                bool[] visited, 
+                int[] disc,
+                int[] low, 
+                int[] parent
+            )
+        {
+
+           
+            int children = 0;
+
+            
+            visited[u] = true;
+
+            
+            disc[u] = low[u] = ++time;
+
+            
+            foreach (int i in graph.AdjacencyList(u))
+            {
+                int v = i; 
+
+            
+                if (!visited[v])
+                {
+                    children++;
+                    parent[v] = u;
+                    APUtil(graph, v, visited, disc, low, parent);
+
+                    
+                    low[u] = Math.Min(low[u], low[v]);
+
+                 
+                    if (parent[u] == -1 && children > 1)
+                        ap.Add(u);
+
+                    
+                    if (parent[u] != -1 && low[v] >= disc[u])
+                        ap.Add(u);
+                }
+
+                // Update low value of u for parent function calls.
+                else if (v != parent[u])
+                    low[u] = Math.Min(low[u], disc[v]);
+            }
+        }
+
+        // The function to do DFS traversal.
+        // It uses recursive function APUtil()
+        public static HashSet<int> GetAP(this Graph_int graph)
+        {
+            // Mark all the vertices as not visited
+            var visited = new bool[graph.VertexCount()];
+            var disc = new int[graph.VertexCount()];
+            var low = new int[graph.VertexCount()];
+            var parent = new int[graph.VertexCount()];
+            
+
+            // Initialize parent and visited, and
+            // ap(articulation point) arrays
+            for (int i = 0; i < graph.VertexCount(); i++)
+            {
+                parent[i] = -1;
+                visited[i] = false;
+                ap.Clear();
+            }
+
+            // Call the recursive helper function to find articulation
+            // points in DFS tree rooted with vertex 'i'
+            for (int i = 0; i < graph.VertexCount(); i++)
+                if (visited[i] == false)
+                    APUtil(graph, i, visited, disc, low, parent);
+
+            // Now ap[] contains articulation points, print them
+    
+            return ap;
+        }
+    }
+}
---
+++ 4/Graphs4/BoruvkaMST.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs4
+{
+    public static class Boruvka
+    {
+ 
+        private static int Find(List<int> parent, int i)
+        {
+            if (parent[i] == i)
+            {
+                return i;
+            }
+            return Find(parent, parent[i]);
+        }
+
+        
+        private static void UnionSet(List<int> parent, List<int> rank,
+                              int x, int y)
+        {
+            int xroot = Find(parent, x);
+            int yroot = Find(parent, y);
+
+          
+            if (rank[xroot] < rank[yroot])
+            {
+                parent[xroot] = yroot;
+            }
+            else if (rank[xroot] > rank[yroot])
+            {
+                parent[yroot] = xroot;
+            }
+
+            
+            else
+            {
+                parent[yroot] = xroot;
+                rank[xroot]++;
+            }
+        }
+
+
+       
+        public static List<(int, int, int)> BoruvkaMST(this Graph_int graph)
+        {
+            var edges = graph.List_Of_Edges();
+            List<int> parent = new List<int>();
+
+            
+            List<int> rank = new List<int>();
+            List<List<int>> cheapest = new List<List<int>>();
+            var result = new List<(int, int, int)>();
+            
+            int numTrees = graph.VertexCount();
+
+
+           
+            for (int node = 0; node < graph.VertexCount(); node++)
+            {
+                parent.Add(node);
+                rank.Add(0);
+                cheapest.Add(new List<int> { -1, -1, -1 });
+            }
+
+           
+            while (numTrees > 1)
+            {
+
+                
+                for (int i = 0; i < edges.Count;i++)
+                    
+                {
+
+                   
+                    int u = edges[i].Item1, v = edges[i].Item2,
+                        w = edges[i].Item3;
+                    int set1 = Find(parent, u),
+                        set2 = Find(parent, v);
+
+                   
+                    if (set1 != set2)
+                    {
+                        if (cheapest[set1][2] == -1
+                            || cheapest[set1][2] > w)
+                        {
+                            cheapest[set1]
+                                = new List<int> { u, v, w };
+                        }
+                        if (cheapest[set2][2] == -1
+                            || cheapest[set2][2] > w)
+                        {
+                            cheapest[set2]
+                                = new List<int> { u, v, w };
+                        }
+                    }
+                }
+
+               
+                for (int node = 0; node < graph.VertexCount(); node++)
+                {
+
+                    
+                    if (cheapest[node][2] != -1)
+                    {
+                        int u = cheapest[node][0],
+                            v = cheapest[node][1],
+                            w = cheapest[node][2];
+                        int set1 = Find(parent, u),
+                            set2 = Find(parent, v);
+                        if (set1 != set2)
+                        {
+                          
+                            UnionSet(parent, rank, set1, set2);
+                            result.Add((u + 1, v + 1, w));
+                            numTrees--;
+                        }
+                    }
+                }
+                for (int node = 0; node < graph.VertexCount(); node++)
+                {
+                    cheapest[node][2] = -1;
+                }
+            }
+            return result;
+            
+        }
+    }
+}
